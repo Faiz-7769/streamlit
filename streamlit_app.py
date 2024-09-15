a853fc90e31efe62a2582a4fdf22a78f2fcbb0c5
Faiz-7769 <victorious.faizahmed2018@gmail.com>
2024-09-15 13:24:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,12 +22,19 @@ with st.expander("Data Visualization"):
   st.scatter_chart(data = df, x ='bill_length_mm', y = "body_mass_g", color='species')
 
   #Data prepration
-  with st.sidebar:
-    st.header("Input Features")
+with st.sidebar:
+  st.header("Input Features")
     
-    island = st.selectbox('Island',('Biscoe','Dream','Torgesen'))
-    gender = st.selectbox('Sex',('Male','Female'))
-    bill_length_mm = st.slider("Bill length (mm)", 32.1, 59.6, 43.9)
-    bill_depth_mm = st.slider("Bill Depth (mm)", 13.1, 21.5, 17.2)
-    flipper_length_mm = st.slider("Flipper Length (mm)", 172.0, 231.0, 201.0)
-    body_mass_g = st.slider('Weight (g)', 2700.0, 6300.0, 4200.0)
+  island = st.selectbox('Island',('Biscoe','Dream','Torgesen'))
+  gender = st.selectbox('Sex',('Male','Female'))
+  bill_length_mm = st.slider("Bill length (mm)", 32.1, 59.6, 43.9)
+  bill_depth_mm = st.slider("Bill Depth (mm)", 13.1, 21.5, 17.2)
+  flipper_length_mm = st.slider("Flipper Length (mm)", 172.0, 231.0, 201.0)
+  body_mass_g = st.slider('Weight (g)', 2700.0, 6300.0, 4200.0)
+
+#create a Data Frame for the einput features
+  data = {'island':island,'bill_length_mm':bill_length_mm,'bill_depth_mm':bill_depth_mm,
+         'flipper_length_mm':flipper_length_mm,'body_mass_g':body_mass_g, 'Gender':gender}
+  input_df = pd.Dataframe(data, index[0])
+  input penguins = pd.concat([input_df, X], axis = 0)
+input_df
