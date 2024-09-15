487dc1ca52eaf1e50997bd319eb94b5340b1714f
Faiz-7769 <victorious.faizahmed2018@gmail.com>
2024-09-15 13:42:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,16 @@ with st.sidebar:
           'bill_depth_mm':bill_depth_mm,
          'flipper_length_mm':flipper_length_mm,
           'body_mass_g':body_mass_g,
-          'Gender':gender}
+          'sex':gender}
   input_df = pd.DataFrame(data, index=[0])
   input_penguins = pd.concat([input_df, X], axis = 0)
-input_df
+with st.expander('Input Features'):
+  st.write('**Input Penguin**')
+  input_df
+  st.write("**Combined Data**")
+  input_penguins
+
+#Encode
+encode = ["island", "sex"]
+df_penguins = pd.get_dummies(input_penguins, prefix = encode)
+df_penguins
