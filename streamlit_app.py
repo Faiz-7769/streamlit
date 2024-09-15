3a636cdf731bf45dc5a655b6848d9226a3163aa2
Faiz-7769 <victorious.faizahmed2018@gmail.com>
2024-09-15 12:13:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,7 @@ import streamlit as st
 
 st.title('Penguin - Classifier')
 
-st.write('Hello world!')
+st.info('This is a Machine Learning Model which Classifies the Penguin')
+
+df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
+df
