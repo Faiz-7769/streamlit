50f6a69b5d854c0618cad2e95637438b4a71cd57
Faiz-7769 <victorious.faizahmed2018@gmail.com>
2024-09-15 13:24:56 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,5 +36,5 @@ with st.sidebar:
   data = {'island':island,'bill_length_mm':bill_length_mm,'bill_depth_mm':bill_depth_mm,
          'flipper_length_mm':flipper_length_mm,'body_mass_g':body_mass_g, 'Gender':gender}
   input_df = pd.Dataframe(data, index[0])
-  input penguins = pd.concat([input_df, X], axis = 0)
+  input_penguins = pd.concat([input_df, X], axis = 0)
 input_df
