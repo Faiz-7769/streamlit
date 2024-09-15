441bb33c6445db970f3ca3d47e73eabb70fb9c8b
Faiz-7769 <victorious.faizahmed2018@gmail.com>
2024-09-15 12:19:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,6 @@ st.title('Penguin - Classifier')
 st.info('This is a Machine Learning Model which Classifies the Penguin')
 
 with st.expander('Data'):
-  st.write('**Raw Data**')
+  st.write('Raw Data')
   df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
   df
